69f3d65737edf3ee79da668e03c51460a848ae85
Jussi Asp <jussi.asp@helsinki.fi>
2020-11-10 23:42:19 +0200

    viikkoo2

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/KassapaateTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/KassapaateTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class MaksukorttiTest {
+public class KassapaateTest {
 
     Maksukortti kortti;
 
@@ -17,4 +17,40 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }    
+    
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(200);
+        assertEquals("saldo: 2.10", kortti.toString());
+    }    
+    
+    @Test
+    public void saldoVaheneeJosRahaaOnTarpeeksi() {
+        kortti.otaRahaa(10);
+        assertEquals("saldo: 0.0", kortti.toString());
+    }    
+
+    @Test
+    public void saldoEiMuutuJosRahaaEiOleTarpeeksi() {
+        kortti.otaRahaa(11);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }    
+
+    @Test
+    public void rahanOttaminenPalauttaaTrueJosRiitti() {
+        Boolean totuus = kortti.otaRahaa(1);
+        assertEquals("true", totuus.toString());
+    }    
+
+    @Test
+    public void rahanOttaminenPalauttaaFalseJosEiRiittanyt() {
+        Boolean totuus = kortti.otaRahaa(11);
+        assertEquals("false", totuus.toString());
+    }    
+    
 }
--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,40 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }    
+    
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(200);
+        assertEquals("saldo: 2.10", kortti.toString());
+    }    
+    
+    @Test
+    public void saldoVaheneeJosRahaaOnTarpeeksi() {
+        kortti.otaRahaa(10);
+        assertEquals("saldo: 0.0", kortti.toString());
+    }    
+
+    @Test
+    public void saldoEiMuutuJosRahaaEiOleTarpeeksi() {
+        kortti.otaRahaa(11);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }    
+
+    @Test
+    public void rahanOttaminenPalauttaaTrueJosRiitti() {
+        Boolean totuus = kortti.otaRahaa(1);
+        assertEquals("true", totuus.toString());
+    }    
+
+    @Test
+    public void rahanOttaminenPalauttaaFalseJosEiRiittanyt() {
+        Boolean totuus = kortti.otaRahaa(11);
+        assertEquals("false", totuus.toString());
+    }    
+    
 }
